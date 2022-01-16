8852954936cdb4ec261f24f8d6e23ee1cdfa6036
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-01-16 23:19:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React
+
+Starting template app for react
---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useTest } from "../hooks";
+
+const App = () => {
+	const [status, color] = useTest();
+
+	return (
+		<>
+			<h1 className="ctn-20-20">Hello from React !</h1>
+			<p className="ctn-0-20">
+				Service status: <span style={{ color: color }}>{status}</span>
+			</p>
+			<div className="ctn-20-20">
+				<p className="prg">
+					Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
+					consectetur. Lorem <span className="red">ipsum dolor</span> sit amet
+					consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit
+					amet consectetur. Lorem ipsum dolor sit amet consectetur.{" "}
+					<span className="bold">Lorem ipsum</span> dolor sit amet consectetur. Lorem
+					ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur.
+					<br />
+					<br />
+					<span className="italic">Lorem ipsum dolor sit amet consectetur.</span>
+				</p>
+			</div>
+			<div className="ctn-0-20">
+				<button className="btn-alt mr-20">Button 1</button>
+				<button className="btn">Button 2</button>
+			</div>
+		</>
+	);
+};
+
+export default App;
