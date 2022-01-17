296dd49d9e601da141d7e4e69708545ad1a8490e
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-01-17 14:52:54 +0100

    update demo app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,6 +27,15 @@ const App = () => {
 				<button className="btn-alt mr-20">Button 1</button>
 				<button className="btn">Button 2</button>
 			</div>
+			<div className="col-10 col-center p-center bg-hex00000066">
+				<div className="modal__window ctn-20-20 br-10 bg-white">
+					<h2 className="mb-20">This is a modal !</h2>
+					<p className="prg">Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
+					consectetur. Lorem <span className="red">ipsum dolor</span> sit amet
+					consectetur. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit
+					amet consectetur. Lorem ipsum dolor sit amet consectetur.</p>
+				</div>
+			</div>
 		</>
 	);
 };
